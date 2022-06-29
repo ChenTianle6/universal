de0258f5503fc28ddf75124e50b46563db4a07a1
ctl <3054008136@qq.com>
2022-06-29 17:16:55 +0800

    feat(layout): 修复声明变量问题

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>
-    appmain
-  </div>
+  <div class="header">个人中心</div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-
+.header{
+  margin: 10px;
+  height: 20px;
+  line-height: 20px;
+  color: #fff;
+  width: 50px;
+  background-color: #304156;
+  font-size: 12px;
+  padding: 0 20px;
+}
 </style>
--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
-    navbar
+  <div class="header">
+    个人中心
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
-
+.header{
+  height: 50px;
+  background-color: darkgrey;
+  line-height: 50px;
+  padding-left: 30px;
+}
 </style>
