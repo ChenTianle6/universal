647329e2d01765795ae76e7040e92c1a1e49662b
ctl <3054008136@qq.com>
2022-06-29 18:39:16 +0800

    feat(menu): 使用递归组件实现菜单的渲染

--- src/layout/AppMain.vue
+++ src/layout/AppMain.vue
@@ -5,14 +5,5 @@
 <script setup></script>
 
 <style lang="scss" scoped>
-.header{
-  margin: 10px;
-  height: 20px;
-  line-height: 20px;
-  color: #fff;
-  width: 50px;
-  background-color: #304156;
-  font-size: 12px;
-  padding: 0 20px;
-}
+
 </style>
