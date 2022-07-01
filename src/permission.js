556551980f084accc293b0cadb1eb8326e66711b
ctl <3054008136@qq.com>
2022-07-01 20:46:38 +0800

    feat(router and permission): 动态添加路由表

--- src/permission.js
+++ src/permission.js
@@ -1,15 +1,3 @@
-/**
- *
- * 登录 token
- *  进入到登录  跳转到登录之前的页面
- *  进入到登录以后页面  可以进入
- *
- * 未登录
- *  没有token
- *    进入的是登录 进入
- *    进入的不是登录页 跳转到登录页
- *
- */
 import router from './router'
 import store from './store'
 
@@ -19,23 +7,28 @@ const whiteList = ['/login']
 router.beforeEach(async (to, from, next) => {
   // 获取token
   const token = store.getters.token
-  // 获取用户信息
-  const userInfo = store.getters.userInfo
   if (token) {
     if (to.path === '/login') {
       next(from.path)
     } else {
-      if (userInfo) {
-        next()
-      } else {
+      if (!store.getters.hasUserInfo) {
         // 调用获取信息接口
         const response = await store.dispatch('user/getUserInfo')
         if (response) {
-          next()
+          const { permission } = response
+          const filterRoutes = await store.dispatch(
+            'permission/filterRoutes',
+            permission.menus
+          )
+          filterRoutes.forEach((item) => {
+            router.addRoute(item)
+          })
+          return next(to.path)
         } else {
           next('/login')
         }
       }
+      next()
     }
   } else {
     if (whiteList.includes(to.path)) {
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import user from './modules/user'
 import getters from './getters'
+import Permission from './modules/permission'
 export default createStore({
   getters,
   modules: {
-    user
+    user,
+    Permission
   }
 })
